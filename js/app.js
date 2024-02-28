5b248b46ff6dce1a78d43a8377950f14deb6b50f
Farjana-Fariha <farjanafariha507@gmail.com>
2024-02-28 18:54:26 +0600

    show more and loading spinner done

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 const loadPhones = async (searchedText) => {
    const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchedText}`);
    const data = await res.json();
-   console.log( searchedText, data.data);
    displayData(data.data);
 
 
@@ -9,8 +8,16 @@ const loadPhones = async (searchedText) => {
 const displayData = phones =>{
    const projectContainer = document.querySelector('.project-container');
    projectContainer.textContent = "";
+   // display show more btn 
+   const showMoreBtn = document.getElementById('show-more-btn');
+   if(phones.length > 10){
+      showMoreBtn.classList.remove('hidden')
+   }else{
+      showMoreBtn.classList.add('hidden')
+   }
+   // display products
+   phones = phones.slice(0,10)
    phones.forEach(phone =>{
-      console.log(phone);
       const phoneCard = document.createElement('div');
       phoneCard.classList ='phone-card';
       phoneCard.innerHTML = `
@@ -27,12 +34,22 @@ const displayData = phones =>{
     </div>
       `;
       projectContainer.appendChild(phoneCard);
-      
-
-   })
+   });
+   loading(false)
 }
+// search item
 const search = () => {
+   loading(true)
    const inputFeild = document.getElementById('searchedItem');
    const searchedText = inputFeild.value;
    loadPhones(searchedText)
+}
+// show loading
+const loading = (isLoading) => {
+   const loading = document.getElementById('loading');
+   if(isLoading){
+      loading.classList.remove('hidden')
+   }else{
+      loading.classList.add('hidden');
+   }
 }
